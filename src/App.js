5124f3d9b96455648f766d0b2927faa8522a5cc2
AeolusZane <1393649219@qq.com>
2023-02-05 00:58:48 +0800

    app fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch('https://cloudflare.1393649219.workers.dev/')
+      .then(r => r.json()).then(r => {
+        setText(r.hello || 'Error');
+        setData(r || {
+          data: "data error",
+          time: "time error"
+        })
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data.time}
         </p>
         <a
           className="App-link"
@@ -15,8 +29,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {data.data}
         </a>
+        <p>
+          {'Hello '+ text}
+        </p>
       </header>
     </div>
   );
